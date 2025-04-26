625ce7b54aac8b2977481cc62eec14b34f46c142
Abuzar <abuzarkhan1242@gmail.com>
2025-04-26 16:41:55 +0500

    Jwt done

---
+++ src/main/java/com/abuzar/springsecuritywithjwt/entities/JwtResponse.java
@@ -0,0 +1,15 @@
+package com.abuzar.springsecuritywithjwt.entities;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class JwtResponse {
+
+    private String jwtToken;
+    private String username;
+}
